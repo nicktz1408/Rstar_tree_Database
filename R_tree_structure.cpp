ac1b7bde2650948847da448b2490956006c09b9a
Emmanouil Eleftheriadis <eemmanoua@csd.auth.gr>
2021-05-09 13:05:32 +0300

    Nodes structures

--- R_tree_structure.cpp
+++ R_tree_structure.cpp
@@ -2,15 +2,25 @@
 #include <vector>
 #include "Entity.cpp"
 
+
+
+#define M 50
+#define m 25
+
 using namespace std;
 
 /**
 ** Struct for Points represantation
 **/
 struct Point{
-    int x;
-    int y;
+    double x;
+    double y;
 };
+
+struct Index{
+    int numOfBlock;
+    int numOfLine;
+}
 
 /**
 ** Class for the Rectangles represantation
@@ -19,6 +29,7 @@ class Rectangle{
     public:
         Point a;
         Point b;
+        BaseNode *childNode;
         Rectangle();
         Rectangle(Point x, Point y){
             a = x;
@@ -27,11 +38,50 @@ class Rectangle{
         
 };
 
+class BaseNode{
+    BaseNode();
+}
+
+class Node: BaseNode{
+    Node() {
+        boundingBoxes = vector <Rectangle *>(M);
+    }
+    vector<Rectangle *> boundingBoxes;
+}
+class NodeLeaf: BaseNode{
+    NodeLeaf() {
+        arr = vector <Index>(M);
+        points = (Point *) malloc(M*sizeof(Point));
+    }
+
+    vector<Index> arr;
+    Point *points;
+}
+
 
 class Rtree{
     public:
-        Rtree();
-        void insert(Entity newRecord);
+        Rtree(){
+            Node *root = NULL;
+        }
+
+
+        void insert(Entity newRecord){
+            Node *currentNode = root;
+            Point p((double) newRecord.getLan(), (double) newRecord.getLon());
+            insertUtility(currentNode, p);
+        }
+
+        NodeLeaf *insertUtility(Node *node, Point p){
+            for(int i=0;i<node.size();i++){
+                if(contains(p, node->boundingBoxes[i])){
+                    currentNode = node->boundingBoxes[i]->childNode;
+                    i = 0;
+                }
+            }
+        }
+
+
         vector<Entity> rangeQuery(double range);
         vector<Entity> knnQuery(int neighbors);
 
@@ -39,6 +89,7 @@ class Rtree{
 
 
     private:
+        Node *root;
         //In this function we check if two rectangles overlapping
         bool overlap(Rectangle m, Rectangle n){
             return (max(m.a.x,n.a.x) <= min(m.b.x,n.b.x) && max(m.a.y,n.a.y) <= min(m.b.y,n.b.y));
